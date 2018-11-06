233f2d4e2ac62e546f8def5a278501f4b082b34f
Jon Gray <jgray@spartaglobal.com>
2018-11-06 16:53:04 +0000

    v0.7 adding sign up username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_06_120613) do
+ActiveRecord::Schema.define(version: 2018_11_06_161644) do
 
   create_table "cards", force: :cascade do |t|
     t.string "name"
@@ -54,11 +54,12 @@ ActiveRecord::Schema.define(version: 2018_11_06_120613) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
